e48e4bf3f02e89d982a7b2ddaefeedad4d584c71
Christopher Hlubek <hlubek@networkteam.com>
2012-11-27 18:25:56 +0100

    [TASK] Add preliminary session rest service for touch

--- Classes/TYPO3/SingleSignOn/Server/Controller/SessionController.php
+++ Classes/TYPO3/SingleSignOn/Server/Controller/SessionController.php
@@ -29,12 +29,35 @@ class SessionController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	protected $supportedMediaTypes = array('application/json');
 
 	/**
+	 * Get authentication information from a session
 	 *
+	 * GET /sso/session/xyz-123
 	 *
-	 * @param string $sessionId
+	 * @param string $sessionId The session id
 	 */
 	public function showAction($sessionId) {
 
+	}
+
+	/**
+	 * Touch a session to refresh the last active timestamp
+	 *
+	 * POST /sso/session/xyz-123/touch
+	 *
+	 * @param string $sessionId The session id
+	 */
+	public function touchAction($sessionId) {
+		// TODO Touch actual session using the SessionManager
+	}
+
+	/**
+	 * DELETE /sso/session/xyz-123
+	 *
+	 * @param string $sessionId The session id
+	 */
+	public function destroyAction($sessionId) {
+		// TODO Remove the session using the SessionManager
+		// TODO Notify clients that are registered in the session
 	}
 
 }
