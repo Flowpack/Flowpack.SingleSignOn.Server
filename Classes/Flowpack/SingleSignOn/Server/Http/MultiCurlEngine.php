66dc894587939034f961dfffa8103d5b97d3b545
Alexander Berl <berl@trackmyrace.com>
2017-04-06 10:52:13 +0200

    TASK: Apply migration TYPO3.Flow-201209251426
    Rename FLOW3 to TYPO3 Flow

--- Classes/Flowpack/SingleSignOn/Server/Http/MultiCurlEngine.php
+++ Classes/Flowpack/SingleSignOn/Server/Http/MultiCurlEngine.php
@@ -5,7 +5,7 @@ namespace Flowpack\SingleSignOn\Server\Http;
  * This script belongs to the TYPO3 Flow package "Flowpack.SingleSignOn.Server". *
  *                                                                               */
 
-use TYPO3\Flow\Annotations as FLOW3;
+use TYPO3\Flow\Annotations as Flow;
 
 use TYPO3\Flow\Http\Request,
 	TYPO3\Flow\Http\Response;
